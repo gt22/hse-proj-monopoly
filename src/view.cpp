20c07ad1a1f20ec9a11a3e8f1e18216aa88a08bf
xbreathoflife <xbreathoflife@gmail.com>
2020-03-04 02:32:48 +0300

    Fix field

--- src/view.cpp
+++ src/view.cpp
@@ -12,65 +12,35 @@ NcursesView::~NcursesView() {
 }
 
 void NcursesView::printGrid() {
-    move(0, 140);
-    printw("A");
-    for (int line = 0; line <= 38; line += 38)
+    /*bottom and top lines*/
+    for (int line = 0; line <= 40; line += 40) {
         for (int y = 0; y <= 4; y+=4)
             for (int x = 0; x <= 14 * 10; x++) {
                 move(line + y, x);
                 printw("-");
             }
-            
-    for (int line = 0; line <= 120; line +=120)
+        for (int x = 30; x <= 120; x+=10)
+            for (int y = 0; y <=4; y++) {
+                move(y + line, x);
+                printw("|");
+            }
+    }
+    /*right and left lines*/
+    for (int line = 0; line <= 120; line +=120) {
         for (int x = 0; x <= 20; x += 20)
-            for (int y = 0; y <= 14 * 3; y++) {
+            for (int y = 0; y <= 11 * 4; y++) {
                 move(y, line + x);
                 printw("|");
             }
 
+        for (int y = 8; y <= 40; y+=4)
+            for (int x = 0; x <= 20; x++) {
+                move(y, line + x);
+                printw("-");
+            }
 
-
-
-
-
-
-
-
-
-    //move(45, 30);
-
-    //printw("A");
-   /* for (int y = 0; y < 2 * 4; y+=4)
-        for (int x = 0; x <= 14 * 10; x++) {
-            move(y, x);
-            printw("-");
-        }               
-    for (int x = 0; x <= 14 * 10; x+=10)
-        for (int y = 0; y < 2 * 5; y++) {
-            move(y, x);
-            printw("|");
-        }*/
-    //move(30, 30);
-    //printw("A");
-        
-    /*for (int x = 0; x <= FIELD_SIZE * 8; x+=8)
-        for(int y = 0; y <= FIELD_SIZE * 4; y++) {
-            move(y, x);
-            printw("|");
-        }
-
-    for (int y = 0; y <= FIELD_SIZE * 4; y+=4)
-        for (int x = 0; x <= FIELD_SIZE * 8; x++) { 
-            move(y, x);
-            printw("-");
-        }
-    
-    for (int y = 0; y <= FIELD_SIZE * 4; y+=4)
-        for (int x = 0; x <= FIELD_SIZE * 8; x+=8) {
-            move(y, x);
-            printw("+");
-        }*/
-    
+    }
+  
     refresh();
 }
 
