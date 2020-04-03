7b7b448ccfaecdad2b635b9482c777619bd9f92f
osboxes.org <xbreathoflife@gmail.com>
2020-04-03 11:01:39 -0400

    write processRequest function

--- include/View.h
+++ include/View.h
@@ -24,16 +24,12 @@ public:
    virtual void redraw(const Board& board) = 0;
    virtual PlayerReply processRequest(Player& p, PlayerRequest req) = 0;
    virtual void processMessage(Player& p, PlayerMessage mes) = 0;
-  // virtual void getCoord(int &x, int &y, int index);
-
-   // virtual Input::InputAction getAction(); //TODO: как православно обработать экшены
 };
 
 class NcursesView final : public MonopolyView {
 public:
     explicit NcursesView(Manager& manager);
     ~NcursesView() override;
-    bool getActionAlpha();
     void printGrid();
     void runGame();
     void getCoord(int &x, int &y, int index);
@@ -46,7 +42,7 @@ public:
     void changeFieldSize();
 
     int transformCoord(int x, int y);
-    int mouseAction();
+    void processRequestTest();
 private:
     const static int TILES_PER_LINE = 11;
     int maxX = 0;
